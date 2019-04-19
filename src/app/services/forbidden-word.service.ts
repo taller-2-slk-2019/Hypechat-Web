12a86b77dde289eed56be7f37a2d665f4ea4d7fa
matias feld <matias.feld@mdevz.com>
2019-04-19 15:12:23 -0300

    fixed url bugs

--- src/app/services/forbidden-word.service.ts
+++ src/app/services/forbidden-word.service.ts
@@ -4,31 +4,29 @@ import { ForbiddenWord } from '../models/ForbiddenWord';
 import { ServerService } from './server.service';
 import {Observable} from 'rxjs';
 
-const SLASH = '/';
-
 @Injectable({
   providedIn: 'root'
 })
 export class ForbiddenWordService {
-  private extension = '/forbiddenWords';
+  private url = 'forbiddenWords';
 
   constructor(private http: HttpClient, private serverService: ServerService) { }
 
   getForbiddenWords(): Observable<any> {
-    const params = new HttpParams().set('organizationId', '1');
-    return this.serverService.get<ForbiddenWord[]>(this.extension, params);
+    const params = new HttpParams().set('organizationId', '1');  //TODO harcoded id
+    return this.serverService.get<ForbiddenWord[]>(this.url, params);
   }
 
   addForbiddenWord(newWord: string) {
     const data = {
       word: newWord,
-      organizationId: '1'
+      organizationId: '1'  //TODO harcoded id
     };
-    return this.serverService.post(this.extension, data);
+    return this.serverService.post(this.url, data);
   }
 
   deleteForbiddenWord(deleteWord: number) {
-    const extension = this.extension + SLASH + deleteWord.toString();
-    return this.serverService.delete(extension);
+    const url = `${this.url}/${deleteWord.toString()}`;
+    return this.serverService.delete(url);
   }
 }
