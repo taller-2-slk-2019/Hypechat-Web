4b5e8a0684a029b5906082dd4446f5d948da25f7
matias feld <matias.feld@mdevz.com>
2019-04-28 15:05:23 -0300

    added admin token header

--- src/app/services/server.service.ts
+++ src/app/services/server.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { ForbiddenWord } from '../models/ForbiddenWord';
 import { catchError } from 'rxjs/operators';
@@ -14,18 +14,28 @@ export class ServerService {
   constructor(private http: HttpClient) { }
 
   get<T>(extension, params = {}) {
-    return this.http.get(this.baseUrl + extension, { params }).pipe(catchError(this.handleError));
+    return this.http.get<T>(this.baseUrl + extension, this.getHeaders(params)).pipe(catchError(this.handleError));
   }
 
   post<T>(extension, data = {}) {
-    return this.http.post<ForbiddenWord>(this.baseUrl + extension, data).pipe(catchError(this.handleError));
+    return this.http.post<T>(this.baseUrl + extension, data, this.getHeaders()).pipe(catchError(this.handleError));
   }
 
   delete(extension) {
-    return this.http.delete(this.baseUrl + extension).pipe(catchError(this.handleError));
+    return this.http.delete(this.baseUrl + extension, this.getHeaders()).pipe(catchError(this.handleError));
   }
 
   handleError(error: HttpErrorResponse) {
     return throwError(error.message || 'Error');
   }
+
+  private getHeaders(params = null) {
+    let headers = new HttpHeaders().set('adminToken', 'uS3Y91RLpnpmTa7L83m6'); //TODO harcoded token, implement login
+
+    let result: any = {headers: headers};
+    if (params){
+      result.params = params;
+    }
+    return result;
+  } 
 }
