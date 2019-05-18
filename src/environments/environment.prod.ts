b17e47033ffcb1517770f43bb9b6e41529324a5b
aguszorza <aguszorza@gmail.com>
2019-05-18 15:26:20 -0300

    firebase api key for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://slack-taller2.herokuapp.com/'
+  baseUrl: 'https://slack-taller2.herokuapp.com/',
+  firebaseConfig: {
+    apiKey: "AIzaSyDazVK7i04idMVm3ymmTRum1n8_uXVZ8ag",
+    authDomain: "hypechat-2fee8.firebaseapp.com",
+    databaseURL: "https://hypechat-2fee8.firebaseio.com",
+    projectId: "hypechat-2fee8",
+    storageBucket: "hypechat-2fee8.appspot.com",
+    messagingSenderId: "1009732394195",
+    appId: "1:1009732394195:web:8c24b08dca5353de"
+  }
 };
